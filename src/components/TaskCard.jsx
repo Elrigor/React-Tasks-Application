f7cc31fb1eeaa0fede86ce66610a4faf9bdc2e5c
Robert <robert.maracine25@gmail.com>
2023-01-11 16:05:13 +0100

    added Context and Tailwind

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
-function TaskCard({ task, deleteTask }) {
+function TaskCard({ task }) {
+  const { deleteTask } = useContext(TaskContext);
   return (
     <div>
       <h1>{task.title}</h1>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 import TaskCard from "./TaskCard";
 
-function TaskList({ tasks, deleteTask }) {
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
   if (tasks.length == 0) {
     return "No hay tareas aún";
   }
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask} />
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
