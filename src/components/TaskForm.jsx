1edc09c1285bdff6ad34629dbfcd1f5d9d63af04
Robert <robert.maracine25@gmail.com>
2023-01-11 16:52:19 +0100

    added tailwind styles

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -14,20 +14,25 @@ function TaskForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Escribe tu título"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder="Escribe tu descripción"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-      />
-      <button>Guardar</button>
-    </form>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb3">Crea tu tarea</h1>
+        <input
+          className="bg-slate-300 p-3 w-full mb2"
+          placeholder="Escribe el título de la tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          autoFocus
+        />
+        <textarea
+          className="bg-slate-300 p-3 w-full mb2"
+          placeholder="Escribe la descripción de la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <button className="bg-indigo-500 px-3 py-1 text-white text-center">Guardar</button>
+      </form>
+    </div>
   );
 }
 
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,10 +6,10 @@ function TaskList() {
   const { tasks } = useContext(TaskContext);
 
   if (tasks.length == 0) {
-    return "No hay tareas aún";
+    return <h1 className="text-white text-4xl font-bold text-center">No hay tareas aún</h1>;
   }
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-2">
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} />
       ))}
